420a25b517106e7d20d917920a97bba8793ef876
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2018-06-19 12:28:49 +0700

    add syns

--- configdb/sequelize.js
+++ configdb/sequelize.js
@@ -8,48 +8,13 @@ const sequelize = new Sequelize(config.server, config.username, config.password,
         timestamps: true
     },
 });
-
+sequelize.sync()
 const op = Sequelize.Op;
 
 const model = {};
 model.Sequelize = Sequelize;
 model.sequelize = sequelize;
 model.op = op;
-sequelize.sync()
-    .then(() => model.article.create({
-        id: '',
-        title: 'header title',
-        image: 'img64bit',
-        des:'dfdsfsdfsdfdsgfdg',
-        createdAt: '',
-        updatedAt: ''
-    }))
-    .then(() => model.product.create({
-      id: '',
-      name: 'yeddsdf',
-      image: 'http://via.placeholder.com/350x150',
-      type: 1,
-      des: 'asdadad',
-      price:200,
-      createdAt: '',
-      updatedAt: ''
-  }))
-    .then(() => model.cart.create({
-        id: '',
-        productId: 1,
-        count: 2,
-        checkOut: 1,
-        createdAt: '',
-        updatedAt: ''
-    }))
-
-    .then(() => model.project.create({
-        id: '',
-        name: 'asdsadsa',
-        type: 1,
-        createdAt: '',
-        updatedAt: ''
-    }))
 
 model.article =  require('../models/article')(sequelize, Sequelize);
 model.cart =  require('../models/cart')(sequelize, Sequelize);
